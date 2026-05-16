dd4abd57829e97fefc2031c71364d1ec6af86611
pekerleke <mrtnsalem@gmail.com>
2026-05-16 16:18:22 -0300

    add new rgb matrix

---
+++ keyboards/lemokey/l3/iso/keymaps/pekerleke/config.h
@@ -0,0 +1,9 @@
+/* Keymap config: pekerleke (Lemokey L3 ISO)
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+/* First boot after flashing / EEPROM reset: use custom effect (cyclable via RGB_MOD). */
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_CUSTOM_pek_fn_row
